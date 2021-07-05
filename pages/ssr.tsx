75cecb8299b6bdff8350b47976768b4596160fff
Steven Wu <wuichen@users.noreply.github.com>
2021-07-05 13:39:18 +0800

    ssr

--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -1,9 +1,10 @@
-import Index from "./index";
+import { getSession } from 'next-auth/client';
+import Index from './index';
 
-export default function SSR() {
-  return <Index />;
+export default function SSR({ session }) {
+  return <Index session={session} />;
 }
 
-export function getServerSideProps() {
-  return { props: { initialState: 1000 } };
+export async function getServerSideProps(ctx) {
+  return { props: { initialState: 1000, session: await getSession(ctx) } };
 }
